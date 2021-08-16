05cb0137a85ba2528e8abd7ea0bf8a6671c6e854
harjoth <harjoth.khara@gmail.com>
2021-08-15 19:30:52 -0700

    compiling typescript

---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+const character = 'luigi';
+
+console.log(character)
+
+const inputs = document.querySelectorAll('input')
+
+console.log(inputs)
+
+inputs.forEach(input => {
+  console.log(input)
+})
+
+// compiling - tsc <filename> -w  // w -> watches the ts file for changes
