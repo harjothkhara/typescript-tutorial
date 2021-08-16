024a3c60b37c6287efbbe274d79ebc54f7f1f1ad
harjoth <harjoth.khara@gmail.com>
2021-08-15 19:44:39 -0700

    type basics

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,15 @@
-var character = 'luigi';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
-// compiling - tsc <filename> -w  // w -> watches the ts file for changes
+// TS uses strict types and JS does not
+var character = 'mario'; // always be a string
+var age = 30; // always be a number
+var isBlackBelt = false; // always be a boolean
+// character = 20 [NO]
+character = 'luigi';
+//age = 'yoshi [NO]
+age = 40;
+//isBlackBelt = 'yes' [NO]
+isBlackBelt = true;
+// won't compile if wrong type is passed in, cleaner code, less errors
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,20 @@
-const character = 'luigi';
+// TS uses strict types and JS does not
+let character = 'mario'; // always be a string
+let age = 30; // always be a number
+let isBlackBelt = false // always be a boolean
 
-console.log(character)
+// character = 20 [NO]
+character = 'luigi'
 
-const inputs = document.querySelectorAll('input')
+//age = 'yoshi [NO]
+age = 40
 
-console.log(inputs)
+//isBlackBelt = 'yes' [NO]
+isBlackBelt = true
 
-inputs.forEach(input => {
-  console.log(input)
-})
+// won't compile if wrong type is passed in, cleaner code, less errors
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+}
 
-// compiling - tsc <filename> -w  // w -> watches the ts file for changes
+console.log(circ(7.5))
